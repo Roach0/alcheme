0d65f155207c57a49442e39c6cf32032b04ac667
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2026-06-06 17:48:35 -0500

    f
    f

--- scenes/scene_components/queue/card_queue.gd
+++ scenes/scene_components/queue/card_queue.gd
@@ -2,10 +2,13 @@ class_name CardQueue
 extends MarginContainer
 
 @onready var grid: Dictionary = {
-	1: [null, null, null, null, null],
-	2: [null, null, null, null, null],
-	3: [null, null, null, null, null],
-	4: [null, null, null, null, null],
+	1: [null, null, null, null, null, null, null],
+	2: [null, null, null, null, null, null, null],
+	3: [null, null, null, null, null, null, null],
+	4: [null, null, null, null, null, null, null],
+	5: [null, null, null, null, null, null, null],
+	6: [null, null, null, null, null, null, null],
+	7: [null, null, null, null, null, null, null],
 }
 
 var active_row: int = 1
